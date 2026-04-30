2856fafe940c2594fa78f8e6d3ac4338125705b7
anilton <aniltonmagalhaes@gmail.com>
2026-04-30 00:35:38 -0300

    feat: adiciona models de Cidades e Rotas
    Inclui migrações, página de admin e script que facilita a população do banco com dados iniciais (futuramente serão usados em testes)

--- nucleo/admin.py
+++ nucleo/admin.py
@@ -1,3 +1,32 @@
+# Register your models here.
+# https://docs.djangoproject.com/pt-br/6.0/intro/tutorial07/#customize-the-admin-form
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.City)
+class CityAdmin(admin.ModelAdmin):
+    # campos de listagem
+    list_display = ['name', 'importance', 'count_routes']
+    search_fields = ['name']
+    list_filter = ['importance']
+
+    class RouteInline(admin.TabularInline):
+        model = models.Route
+        fk_name = 'src'
+        extra = 0
+        classes = ['collapse']
+
+    # campos de edição
+    prepopulated_fields = {"slug": ["name"]}
+    inlines = [RouteInline]
+
+    def count_routes(self, obj: models.City):
+        return obj.routes.count()
+    count_routes.short_description = 'número de rotas'
+
+
+@admin.register(models.Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'cost', 'time']
